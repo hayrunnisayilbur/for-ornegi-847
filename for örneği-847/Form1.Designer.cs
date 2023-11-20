2e4011fb11a03a7bd453fa481a2d5d992b341eca
hayrunnisayilbur <145451185+hayrunnisayilbur@users.noreply.github.com>
2023-11-20 10:48:05 +0300

    Add files via upload

---
+++ for örneği-847/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace for_örneği_847
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSayilar = new System.Windows.Forms.ListBox();
+            this.btnAra = new System.Windows.Forms.Button();
+            this.txtAranan = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbSayilar
+            // 
+            this.lbSayilar.FormattingEnabled = true;
+            this.lbSayilar.Location = new System.Drawing.Point(220, 107);
+            this.lbSayilar.Name = "lbSayilar";
+            this.lbSayilar.Size = new System.Drawing.Size(162, 251);
+            this.lbSayilar.TabIndex = 0;
+            // 
+            // btnAra
+            // 
+            this.btnAra.Location = new System.Drawing.Point(197, 68);
+            this.btnAra.Name = "btnAra";
+            this.btnAra.Size = new System.Drawing.Size(101, 23);
+            this.btnAra.TabIndex = 1;
+            this.btnAra.Text = "   ara";
+            this.btnAra.UseVisualStyleBackColor = true;
+            this.btnAra.Click += new System.EventHandler(this.btnAra_Click);
+            // 
+            // txtAranan
+            // 
+            this.txtAranan.Location = new System.Drawing.Point(304, 68);
+            this.txtAranan.Name = "txtAranan";
+            this.txtAranan.Size = new System.Drawing.Size(100, 20);
+            this.txtAranan.TabIndex = 2;
+            this.txtAranan.Text = "                  ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAranan);
+            this.Controls.Add(this.btnAra);
+            this.Controls.Add(this.lbSayilar);
+            this.Name = "Form1";
+            this.Text = "  ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbSayilar;
+        private System.Windows.Forms.Button btnAra;
+        private System.Windows.Forms.TextBox txtAranan;
+    }
+}
+
---
+++ for örneği-847/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace for_örneği_847
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAra_Click(object sender, EventArgs e)
+        {
+            int aranan = Convert.ToInt32(txtAranan.Text);
+            bool bulundu = false;
+            for ( int i = 0; i < 20; i++)
+            {
+                int sayi = Convert.ToInt32(lbSayilar.Items[i]);
+
+                if( sayi == aranan)
+                {
+                    bulundu = true;
+                    break; // bu komut döngüden cıkarır...
+                }
+            }
+
+            if (bulundu)
+            {
+                MessageBox.Show("aranan sayı bulundu");
+            }
+            else
+            {
+                MessageBox.Show("aranan sayı YOK");
+            }
+        }
+    }
+}
